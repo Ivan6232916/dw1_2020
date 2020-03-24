d02b6779c7454512f3e4109bd4eb14fdf6d55acc
Ivan Sanabria <ivansanabria100@gmail.com>
2020-03-24 11:35:30 -0400

    archivos clase 03

---
+++ clase03/js/app.js
@@ -0,0 +1,25 @@
+//alert("Atencion");
+var numero = 5
+
+	for (i = 0; i <= 10; i++) {
+		console.log(numero + "x" + i + "=" + numero * i);
+
+	}
+
+var num1 = 5;
+var num2 = 7;
+if (num1 > num2){
+		console.log("ERROR, Modifique los valores..");
+	}
+	
+else{
+	console.log("Tabla del " + num1);
+	for (i = 0; i <= 10; i++) {
+		console.log(num1 + "x" + i + "=" + num1 * i);
+	}
+	console.log("Tabla del " + num2);
+	for (i = 0; i <= 10; i++) {
+		console.log(num2 + "x" + i + "=" + num2 * i);
+	}
+
+}
